ffff8d953c8814484a9771af418cead8b0a8dfda
AnshikaSethiya <anshikasethiya28@gmail.com>
2023-04-11 12:13:04 +0530

    connected with mongodb database

---
+++ resolvers.js
@@ -0,0 +1,64 @@
+import {quotes,users } from "./fakedb.js"
+import {randomBytes} from "crypto"
+import bcrypt from "bcryptjs"
+import mongoose from "mongoose"
+import { JWT_SECRET } from "./config.js"
+import jwt from "jsonwebtoken"
+
+const User = mongoose.model("User")
+const Quote = mongoose.model("Quote")
+
+const resolvers  = {
+    Query:{
+        users : () => users,
+        user:(_,{_id}) => users.find(user => user._id == _id),
+        quotes: () => quotes,
+        iquote:(_,{by}) => quotes.filter(quote => quote.by == by)
+    },
+    User:{
+        quotes:(ur) => quotes.filter(quote => quote.by == ur._id)
+    }, 
+    Mutation:{
+        signupUser: async (_,{userNew}) =>{
+            const user = await User.findOne({email: userNew.email})
+
+            if(user){
+                throw new Error("User already exists!!")
+            }
+
+            const hashedPassword = await bcrypt.hash(userNew.password, 12)
+
+            const newUser = new User({
+                ...userNew,
+                password: hashedPassword
+            })
+
+            return await newUser.save();
+        },
+
+        signinUser: async (_, {userSignin}) => {
+            const user = await User.findOne({email: userSignin.email})
+            if(!user){
+                throw new Error("USer does not exist!!")
+            }
+
+            const passMatch = await bcrypt.compare(userSignin.password, user.password)
+
+            if(!passMatch){
+                throw new Error("invalid username or password!!")
+            } 
+
+            const token = jwt.sign({userId:user._id}, JWT_SECRET)
+            return {token}
+        }, 
+        createQuote: async (_, {name}, {userId}) => {
+            if(!userId) throw new Error("User must log in first!!")
+            const newQuote = new Quote({name, by:userId})
+            await newQuote.save()
+            
+            return "Quote created successfully!! "
+        }
+    }
+}
+
+export default resolvers
